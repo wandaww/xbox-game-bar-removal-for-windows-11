eedc2cc9434d7f13a9a6a90d2bede251934a5d99
wanda agustiansyah r <wandatamvan@gmail.com>
2021-08-05 02:21:52 +0700

    the holly script

---
+++ remove-game-bar.ps1
@@ -0,0 +1,18 @@
+$packages = @(
+"Microsoft.Xbox.TCUI"
+"Microsoft.XboxApp"
+"Microsoft.XboxGameCallableUI"
+"Microsoft.XboxGameOverlay"
+"Microsoft.XboxGamingOverlay"
+"Microsoft.XboxIdentityProvider"
+"Microsoft.XboxLive"
+"Microsoft.XboxSpeechToTextOverlay"
+)
+
+ForEach ($packages in $packages) {
+Get-AppxPackage -Name $packages -AllUsers | Remove-AppxPackage
+
+Get-AppXProvisionedPackage -Online |
+where DisplayName -EQ $packages |
+Remove-AppxProvisionedPackage -Online
+} 
